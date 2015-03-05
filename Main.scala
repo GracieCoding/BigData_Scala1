d41b63498f7c2f7256e1cd09c5004bc1aa1818bf
GracieCoding <GracieCoding@users.noreply.github.com>
2015-03-04 20:25:25 -0500

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -2,9 +2,8 @@
  * Created by Grace & Gabriela.
  */
 
+import scala.collection.mutable.{Map, PriorityQueue}
 import scala.io.Source
-import scala.collection.mutable.PriorityQueue
-import scala.collection.mutable.Map
 
 object Main {
 
@@ -30,14 +29,27 @@ object Main {
     }
   }*/
 
-  def putStuffInQueue(myQueue: PriorityQueue[Data], myList: List[Data]) : Unit = {
-    if (!myList.isEmpty){
-      myQueue.enqueue(myList.head)
-      putStuffInQueue(myQueue, myList.tail)
+  def putStuffInQueue(myQueue: PriorityQueue[Data], k: Int, counter: Field[Int], myData: Data) : PriorityQueue[Data] = {
+    if (counter.value < k) {
+      myQueue.enqueue(myData)
+      counter.value = counter.value + 1
+      myQueue
+    }
+    else {
+      if (myData.getScore() > myQueue.min(Ordering.by(greaterThan)).getScore() ){
+        var tempQueue = myQueue.reverse
+        tempQueue.dequeue()
+        tempQueue = tempQueue.reverse
+        tempQueue.enqueue(myData)
+        tempQueue
+      }
+      else {
+        myQueue
+      }
     }
   }
 
-  def getTopK(myQueue: PriorityQueue[Data], k: Int, arr: Array[Data], index: Field[Int]) : Unit = {
+ /* def getTopK(myQueue: PriorityQueue[Data], k: Int, arr:Array[Data], index: Field[Int]) : Unit = {
     var temp = k;
     if (k > 0){
       temp = temp-1
@@ -45,7 +57,67 @@ object Main {
       index.value = index.value + 1
       getTopK(myQueue.tail, temp, arr, index)
     }
+  }*/
+
+  //Hypergeometric distribution
+  /*
+  def HyperCalculate(popSize: Int, catsizeX:Int,category:String, numK:Int, topK:Array[Data]): Unit = {
+    val numcatTopK= CatXinTopK(category, topK)
+    var cal= Combination(catsizeX)*Combination(popSize-catsizeX)
+    var cal2= cal/(Combination(numcatTopK)*Combination(catsizeX-numcatTopK)*Combination(numK-numcatTopK)*Combination(popSize-catsizeX-numK+numcatTopK)  )
   }
+*/
+  def CatXinTopK(category: String, topK:Array[Data]):Unit = {
+    var count=0;
+    for ( i <- 0 until topK.length) {
+      if(topK(i).getCategory()==category)
+        count=count+1
+    }
+    return count
+  }
+  //returns combination of number
+  def Combination(x: Int):Unit= {
+    if (x<1)
+      return -1;
+    else
+      recurCom(x,1);
+  }
+  def recurCom(x:Int, total:Int):Unit = {
+    if (x>1)
+      recurCom(x-1, total*x)
+    else
+      return total;
+  }
+
+
+  /*
+    def merge_sort(List m) {
+     if length(m) <= 1
+       return m
+     var (left, right) = m.splitAt(length(m)/2)
+     left = merge_sort(left)
+     right = merge_sort(right)
+     merge(left, right)
+    }
+    def merge(left, right) {
+      var list result
+      while (!left.isEmpty and !right.isEmpty) {
+        if first(left) <= first(right)
+          append first(left) to result; left = rest(left)
+        else
+          append first(right) to result; right = rest(right)
+      }
+      while !left.isEmpty{
+        append first(left) to result
+        left = rest(left)
+       }
+      while !right.isEmpty{
+        append first(right) to result
+        right = rest(right)
+      }
+      return result
+    ￼}
+    */
 
   def main(args: Array[String]) : Unit = {
 
@@ -53,7 +125,14 @@ object Main {
     var data = " "
     var dataList : List[Data] = List()
     var categoryNum = new Field(1)
+    var counter = new Field(0)
     var categoryCounter: Option[Int] = None
+    var queue = new PriorityQueue[(Data)]()(Ordering.by(greaterThan))
+
+    //number of top k
+    val k = args(1).toInt
+
+    //pop size
     var N = 0
     var myMap : Map[String,Int] = Map()
     for (line <- Source.fromFile(fileName).getLines()){
@@ -72,23 +151,39 @@ object Main {
         myMap.update(data, myMap(data)+1)
       }
       categoryNum.value = 1
+      queue = putStuffInQueue(queue, k, counter, dataInst)
       N = N+1
     }
 
-    val queue = new PriorityQueue[(Data)]()(Ordering.by(greaterThan))
 
-    putStuffInQueue(queue, dataList)
+   /* var tonySucks = new PriorityQueue[(Data)]()(Ordering.by(greaterThan))
+    var testData = new Data()
+    testData.setCategory("Y")
+    testData.setScore(3.14)
+    tonySucks.enqueue(testData)
+    var testD = new Data()
+    testData.setCategory("X")
+    testData.setScore(13.37)
 
-    val k = args(1).toInt
+    var blah = tonySucks.min(Ordering.by(greaterThan))
+    */
 
-    var topK = new Array[Data](k)
 
-    var index = new Field(0)
+    //var topK = new Array[Data](k)
 
-    getTopK(queue, k, topK, index)
 
-    for (i <-0 until topK.length){
+    //var index = new Field(0)
+
+    //getTopK(queue, k, topK, index)
+
+    /*for (i <-0 until topK.length){
       println(topK(i).getScore())
+    }*/
+
+
+    for (i<- 0 until k){
+      println (queue.max(Ordering.by(greaterThan)).getScore())
+      queue.dequeue()
     }
 
     myMap.keys.foreach {i =>
@@ -96,7 +191,8 @@ object Main {
       println("value: " + myMap(i))
     }
 
+
+
   }
 
 }
-
