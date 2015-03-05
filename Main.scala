467985f90cffee0beb3c1cc3d37cce621349b76a
Gato <gvoll333@gmail.com>
2015-03-04 21:26:29 -0500

    resolved diff, taking on the changed i  made in Gabby

--- Main.scala
+++ Main.scala
@@ -2,9 +2,9 @@
  * Created by Grace & Gabriela.
  */
 
-import scala.io.Source
-import scala.collection.mutable.PriorityQueue
 import scala.collection.mutable.Map
+import scala.collection.mutable.PriorityQueue
+import scala.io.Source
 
 object Main {
 
@@ -12,7 +12,7 @@ object Main {
 
   def isNewCategory(myList: List[Data], x: String, field: Field[Int]): Unit = {
     if (!myList.isEmpty){
-      if (myList.head.getCategory() == x){
+      if (myList.head.getCategory() == x) {
         field.value = 0
       }
       isNewCategory(myList.tail, x, field)
@@ -37,7 +37,7 @@ object Main {
     }
   }
 
-  def getTopK(myQueue: PriorityQueue[Data], k: Int, arr: Array[Data], index: Field[Int]) : Unit = {
+  def getTopK(myQueue: PriorityQueue[Data], k: Int, arr:Array[Data], index: Field[Int]) : Unit = {
     var temp = k;
     if (k > 0){
       temp = temp-1
@@ -47,6 +47,78 @@ object Main {
     }
   }
 
+
+  //Hypergeometric distribution
+
+  def HyperCalculate(popSize: Int, catsizeX:Int,category:String, numK:Int, topK:Array[Data]): Float = {
+    val numcatTopK= CatXinTopK(category, topK)
+    var caltop:Float = Combination(catsizeX) * Combination(popSize-catsizeX) * Combination(popSize-numK) * Combination(numK)
+    var calbot: Float = Combination(popSize) * Combination(numcatTopK) * Combination(catsizeX-numcatTopK) * Combination(numK-numcatTopK)*Combination(popSize-catsizeX-numK+numcatTopK)
+    return caltop/calbot
+  }
+
+  def CatXinTopK(category: String, topK:Array[Data]):Int = {
+    var count=0;
+    for ( i <- 0 until topK.length) {
+      if(topK(i).getCategory()==category)
+        count=count+1
+    }
+    return count
+  }
+  //returns combination of number
+  def Combination(x: Int):Int= {
+    if (x<1)
+      return -1
+    else
+      return recurCom(x,1)
+
+  }
+  def recurCom(x:Int, total:Int):Int = {
+    if (x>1)
+      recurCom(x-1, total*x)
+    else
+      return total
+  }
+
+
+//look at List on web and find length func name
+  def merge_sort(m: List[Data]): List[Data] = {
+   if (m.length <= 1)
+     return m
+   var (left, right) = m.splitAt(m.length / 2)
+
+   left = merge_sort(left)
+   right = merge_sort(right)
+
+   merge(left, right)
+  }
+
+  def merge(left: List[Data], right: List[Data]):List[Data] = {
+    var result= List[Data]()
+    var l= left; var r=right
+    while (!l.isEmpty && !r.isEmpty) {
+      if (l.head.getScore() <= r.head.getScore() ){
+        result= l.head :: result
+        l = l.tail
+      } else {
+        result = r.head :: result
+        r = r.tail
+      }
+    }
+
+    while (!l.isEmpty){
+      result= l.head :: result
+      l = l.tail
+     }
+    while (!r.isEmpty){
+      result = r.head :: result
+      r = r.tail
+    }
+    return result
+  }
+
+
+
   def main(args: Array[String]) : Unit = {
 
     val fileName = args(0)
@@ -54,6 +126,7 @@ object Main {
     var dataList : List[Data] = List()
     var categoryNum = new Field(1)
     var categoryCounter: Option[Int] = None
+    //pop size
     var N = 0
     var myMap : Map[String,Int] = Map()
     for (line <- Source.fromFile(fileName).getLines()){
@@ -78,14 +151,21 @@ object Main {
     val queue = new PriorityQueue[(Data)]()(Ordering.by(greaterThan))
 
     putStuffInQueue(queue, dataList)
-
+    //number of top k
     val k = args(1).toInt
 
     var topK = new Array[Data](k)
 
+
     var index = new Field(0)
 
     getTopK(queue, k, topK, index)
+
+    var HyperMap : Map[String,Float] = Map()
+
+    myMap.keys.foreach { i =>
+      HyperMap += (i -> HyperCalculate(N,myMap(i),i,k,topK))
+    }
 
     for (i <-0 until topK.length){
       println(topK(i).getScore())
@@ -94,7 +174,10 @@ object Main {
     myMap.keys.foreach {i =>
       println("Keys: " + i)
       println("value: " + myMap(i))
+      println("hyperval: " + HyperMap(i))
     }
+
+
 
   }
 
